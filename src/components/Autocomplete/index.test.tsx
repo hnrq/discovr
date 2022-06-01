3ff80b98f61c82ed86276cbdf0b7bc4cb0de7b8b
Henrique Ramos <henriquealberone@outlook.com>
2022-06-01 02:13:28 -0300

    feat: create Autocomplete component

---
+++ src/components/Autocomplete/index.test.tsx
@@ -0,0 +1,73 @@
+import { createSignal } from 'solid-js';
+import { render, fireEvent } from 'solid-testing-library';
+import Autocomplete, { AutocompleteProps } from '.';
+
+const renderAutocomplete = (props: Partial<AutocompleteProps>) => {
+  const [query, setQuery] = createSignal('');
+  return render(() => (
+    <Autocomplete
+      query={query}
+      setQuery={setQuery}
+      placeholder="Search..."
+      {...props}
+    />
+  ));
+};
+
+describe('<Autocomplete />', () => {
+  it('calls setQuery when typing in the input', () => {
+    const setQuery = jest.fn();
+    const placeholder = 'Search...';
+    const value = 'hello';
+    const { getByPlaceholderText } = renderAutocomplete({
+      setQuery,
+      placeholder,
+    });
+    const input = getByPlaceholderText(placeholder) as HTMLInputElement;
+    input.value = value;
+    fireEvent.keyUp(input);
+
+    expect(setQuery).toHaveBeenCalledWith(value);
+  });
+
+  it('renders the items if there is any and the input is focused', () => {
+    const placeholder = 'Search...';
+    const items = ['item 1', 'item 2'];
+    const { getByPlaceholderText, getByText } = renderAutocomplete({
+      items,
+      placeholder,
+    });
+
+    fireEvent.focus(getByPlaceholderText(placeholder) as HTMLInputElement);
+
+    items.forEach((item) => {
+      expect(getByText(item)).toBeInTheDocument();
+    });
+  });
+
+  it('hides the dropdown if the input is blurred', () => {
+    const placeholder = 'Search...';
+    const items = ['item 1', 'item 2'];
+    const { queryByText } = renderAutocomplete({
+      items,
+      placeholder,
+    });
+
+    items.forEach((item) => expect(queryByText(item)).not.toBeInTheDocument());
+  });
+
+  it('receives a custom renderItem function', () => {
+    const placeholder = 'Search...';
+    const items = ['item 1', 'item 2'];
+    const { getAllByTestId } = renderAutocomplete({
+      items,
+      placeholder,
+      autoFocus: true,
+      renderItem: (item: string) => (
+        <div data-testid="custom-item">{item.toString()}</div>
+      ),
+    });
+
+    expect(getAllByTestId('custom-item')).toHaveLength(2);
+  });
+});
---
+++ src/components/Autocomplete/index.tsx
@@ -0,0 +1,56 @@
+import {
+  Accessor,
+  Component,
+  For,
+  Setter,
+  Show,
+  createSignal,
+  JSX,
+} from 'solid-js';
+import { Store } from 'solid-js/store';
+
+import './index.scss';
+
+export interface AutocompleteProps {
+  query: Accessor<string>;
+  setQuery: Setter<string>;
+  items?: Store<unknown[]>;
+  autoFocus?: boolean;
+  renderItem?: (item: unknown) => JSX.Element;
+  placeholder?: string;
+}
+
+const Autocomplete: Component<AutocompleteProps> = ({
+  query,
+  setQuery,
+  autoFocus = false,
+  items = [] as string[],
+  renderItem = (item: unknown) => <li>{item.toString()}</li>,
+  placeholder = '',
+}) => {
+  const [focused, setFocused] = createSignal(autoFocus);
+
+  return (
+    <div class="autocomplete">
+      <div class="input">
+        <span class="material-symbols-outlined">search</span>
+        <input
+          value={query()}
+          onKeyUp={(e) => setQuery(e.currentTarget.value)}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          autocomplete="false"
+          autofocus={autoFocus}
+          placeholder={placeholder}
+        />
+      </div>
+      <Show when={items.length > 0 && focused()}>
+        <div class="dropdown">
+          <For each={items}>{(item) => renderItem(item)}</For>
+        </div>
+      </Show>
+    </div>
+  );
+};
+
+export default Autocomplete;
